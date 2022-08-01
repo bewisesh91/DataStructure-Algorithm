728edb0497276d89e189ebd000d5b5467b03bda4
Seunghyun Mun (문승현) <seunghyun.m@bluehole.net>
2022-08-01 21:28:45 +0900

    C#_Study(Day3) : 1 Week Preparation Kit

---
+++ HackerRank/1 WeeK Preparation Kit/Zi ZagSequence.py
@@ -0,0 +1,21 @@
+def findZigZagSequence(a, n):
+    a.sort()
+    mid = int(n / 2)
+    a[mid], a[n-1] = a[n-1], a[mid]
+
+    st = mid + 1
+    ed = n - 2
+    while(st <= ed):
+        a[st], a[ed] = a[ed], a[st]
+        st = st + 1
+        ed = ed - 1
+
+    for i in range (n):
+        if i == n-1:
+            print(a[i])
+        else:
+            print(a[i])
+    return
+
+a = [1, 2, 3, 7, 6, 5, 4]
+findZigZagSequence(a, 7)
