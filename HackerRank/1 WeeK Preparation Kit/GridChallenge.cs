3182555d3640e8a28b05ae336e745e5755e48b05
Seunghyun Mun (문승현) <seunghyun.m@bluehole.net>
2022-08-02 22:04:17 +0900

    C#_Study(Day4) : 1 Week Preparation Kit

---
+++ HackerRank/1 WeeK Preparation Kit/GridChallenge.cs
@@ -0,0 +1,29 @@
+public static string gridChallenge(List<string> grid)
+{
+    var temp = new List<string>();
+    foreach (var info in grid)
+    {
+        var infoList = info.ToList();
+        infoList.Sort();
+        var newString = String.Join("", infoList);
+        temp.Add(newString);
+    }
+    
+    var result = temp
+                .SelectMany(inner => inner.Select((item, index) => new { item, index }))
+                .GroupBy(i => i.index, i => i.item)
+                .Select(g => g.ToList())
+                .ToList();
+                
+    foreach (var info in result)
+    {
+        var check = info.OrderBy(x => x).ToList();
+        var sorted = String.Join("", check);
+        var original = String.Join("", info);
+
+        if (original != sorted)
+            return "NO";
+    }
+        
+    return "YES";
+}
